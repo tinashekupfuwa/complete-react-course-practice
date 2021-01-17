5af58c2f657ae1aa1e4191f8ac3704aa84a1fcc2
Tinashe Kupfuwa <tinashekupfuwa@Tinashes-MBP.localdomain>
2021-01-17 12:33:10 +0200

    new transfer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => { 
+  return (
+    <>
+      <h1>Hello There</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis tenetur magnam et sequi obcaecati odio velit non id consequatur ad inventore delectus labore quibusdam, voluptatem autem saepe veritatis eligendi laborum eos provident adipisci? Nam ullam hic ducimus quibusdam ea!</p>
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));t
