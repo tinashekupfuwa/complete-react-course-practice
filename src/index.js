e9003e1248bf3c9dca4ddeb15071ccc16ddede7a
tinashekupfuwa <tinashekupfuwa@gmail.com>
2021-01-17 13:01:53 +0200

    slight chnages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
 
 const App = () => { 
   return (
-    <>
-      <h1>Hello There</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perferendis tenetur magnam et sequi obcaecati odio velit non id consequatur ad inventore delectus labore quibusdam, voluptatem autem saepe veritatis eligendi laborum eos provident adipisci? Nam ullam hic ducimus quibusdam ea!</p>
-    </>
+    <h1>This is a react element.</h1>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));t
+
+ReactDom.render(<App />, document.getElementById('root'));
