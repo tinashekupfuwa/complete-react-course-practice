e57c99dd44648bb5b155a2c8383c7e83d02187a1
tinashekupfuwa <tinashekupfuwa@gmail.com>
2021-01-18 06:02:17 +0200

    new transfer

---
+++ src/Machine.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Machine extends React.Component { 
+  render() { 
+    function slotMachine() {
+      return (
+        Math.floor(math.random() * 3) + 1;
+      )
+    }
+    icons[0] = 1;
+    icons[1] = 2;
+    icons[2] = 3;
+
+/* ----------------------------- another Method ----------------------------- */
+
+    this.props.slotChoice;
+    return (
+      <>
+
+      </>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import ReactDOM from 'react-dom'
 
-const App = () => { 
-  return (
-    <h1>This is a react element.</h1>
-  )
+// function getMood() { 
+//   const moods = ['Angry', 'happy', 'sad', 'light', 'silly'];
+//   return moods[Math.floor(Math.random() * moods.length)];
+// }
+
+// class Attitude extends React.Component { 
+//   render() {
+//     return (
+//       <h1>My mood is : {getMood()}</h1>
+//     )
+//   }
+// }
+
+function getNum() {
+  return (Math.floor(Math.random() * 10)) + 1;
 }
 
+class LuckyNumber extends React.Component {
+  
+  render() { 
+    const num = getNum();
+    return (
+      <>
+        <h1>My mood is:{ num} </h1>
+        <p>{num===7||2? 'Sucess' : 'Fail'}</p>
+        
+      </>
+    )
+  }
+ }
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LuckyNumber/>,document.getElementById('root'));
